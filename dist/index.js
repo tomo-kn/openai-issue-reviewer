308224c0c9888b85a5dc6e17481a6f54f9d149b9
tomo-kn <okalemon314@gmail.com>
2023-12-31 21:57:50 +0900

    fix: path again

--- dist/index.js
+++ dist/index.js
@@ -1,12 +1,11 @@
-import { reviewIssue } from "./review-issue";
-
+import { reviewIssue } from "./review-issue.js";
 async function run() {
-  try {
-    await reviewIssue();
-  } catch (error) {
-    console.error(error);
-    process.exit(1);
-  }
+    try {
+        await reviewIssue();
+    }
+    catch (error) {
+        console.error(error);
+        process.exit(1);
+    }
 }
-
 run();
---
+++ src/index.ts
@@ -0,0 +1,12 @@
+import { reviewIssue } from "./review-issue.js";
+
+async function run() {
+  try {
+    await reviewIssue();
+  } catch (error) {
+    console.error(error);
+    process.exit(1);
+  }
+}
+
+run();
